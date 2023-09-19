5b9a55b09cffc7ac2b8e212e335618ae525435ab
서주미 <official.jumee@gmail.com>
2023-09-19 17:11:24 +0900

    Update config link

--- site.config.js
+++ site.config.js
@@ -27,7 +27,7 @@ const CONFIG = {
   },
 
   // CONFIG configration (required)
-  link: "https://jumee.vercel.app/",
+  link: "https://jumee-log.vercel.app/",
   since: 2023, // If leave this empty, current year will be used.
   lang: "ko-KR", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
   ogImageGenerateURL: "https://og-image-korean.vercel.app", // The link to generate OG image, don't end with a slash
